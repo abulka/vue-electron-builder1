3efe49bd6502f9a8daca0f247e11ba452cf3249f
Andy Bulka <abulka@gmail.com>
2021-12-19 13:22:37 +1100

    added timeline component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -21,6 +21,38 @@
           <v-btn>hi andy</v-btn>
         </v-row>
 
+        <v-timeline
+          align-top
+          :dense="$vuetify.breakpoint.smAndDown"
+        >
+          <v-timeline-item
+            v-for="(item, i) in items"
+            :key="i"
+            :color="item.color"
+            :icon="item.icon"
+            fill-dot
+          >
+            <v-card
+              :color="item.color"
+              dark
+            >
+              <v-card-title class="text-h6">
+                Lorem Ipsum Dolor
+              </v-card-title>
+              <v-card-text class="white text--primary">
+                <p>Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.</p>
+                <v-btn
+                  :color="item.color"
+                  class="mx-0"
+                  outlined
+                >
+                  Button
+                </v-btn>
+              </v-card-text>
+            </v-card>
+          </v-timeline-item>
+        </v-timeline>
+
         <p class="subheading font-weight-regular">
           For help and collaboration with other Vuetify developers,
           <br>please join our online
@@ -152,6 +184,26 @@
           href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
         },
       ],
+
+      items: [
+        {
+          color: 'red lighten-2',
+          icon: 'mdi-star',
+        },
+        {
+          color: 'purple darken-1',
+          icon: 'mdi-book-variant',
+        },
+        {
+          color: 'green lighten-1',
+          icon: 'mdi-airballoon',
+        },
+        {
+          color: 'indigo',
+          icon: 'mdi-buffer',
+        },
+      ],
+
     }),
   }
 </script>
