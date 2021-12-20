69bb1fe4a745fa6151715df22403859df6f09797
Andy Bulka <abulka@gmail.com>
2021-12-20 16:05:34 +1100

    added mainProcessFile config

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,10 @@ module.exports = {
   ],
   pluginOptions: {
     electronBuilder: {
+      
+      // https://github.com/nklayman/vue-cli-plugin-electron-builder/issues/504
+      mainProcessFile: 'src/background.js',
+
       builderOptions: {
         // options placed here will be merged with default configuration and passed to electron-builder
         linux: {
