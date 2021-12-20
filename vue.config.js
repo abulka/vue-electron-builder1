664a8b351cb214d6087b38f3dabb7fbdef276d76
Andy Bulka <abulka@gmail.com>
2021-12-21 09:49:28 +1100

    experiment2212

--- vue.config.js
+++ vue.config.js
@@ -6,10 +6,14 @@ module.exports = {
     electronBuilder: {
       
       // https://github.com/nklayman/vue-cli-plugin-electron-builder/issues/504
-      // mainProcessFile: 'src/background.js',
+      mainProcessFile: 'src/background.js',
 
       builderOptions: {
         // options placed here will be merged with default configuration and passed to electron-builder
+
+        files: ['dist_electron/**/*'], // This caused the build error
+        // files: ['dist/**/*'], // This caused the build error
+
         linux: {
           category: "Education",
           target: [
