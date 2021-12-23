9b69f8b29deb3d1f5bffd5d78dd86c7f215bb535
Andy Bulka <abulka@gmail.com>
2021-12-24 09:49:43 +1100

    added auto updater

--- src/background.js
+++ src/background.js
@@ -3,6 +3,7 @@
 import { app, protocol, BrowserWindow } from 'electron'
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
 import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
+import { autoUpdater } from 'electron-updater'
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
 // Scheme must be registered before the app is ready
@@ -33,6 +34,8 @@ async function createWindow() {
     // Load the index.html when not in development
     win.loadURL('app://./index.html')
   }
+  autoUpdater.checkForUpdatesAndNotify()
+  console.log('after checkForUpdatesAndNotify')
 }
 
 // Quit when all windows are closed.
